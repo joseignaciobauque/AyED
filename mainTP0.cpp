9f6d9fd26b5fad144d2b86d7e4fd94f6ca9a245c
Jose Ignacio <jirastorzabauque@frba.utn.edu.ar>
2024-09-26 11:34:37 -0300

    mainTP0.cpp

---
+++ mainTP0.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    // Mostrar el mensaje "Hola mundo"
+    std::cout << "Hola mundo" << std::endl;
+    
+    // El cursor ya queda en la siguiente línea gracias a std::endl
+    return 0;
+}
